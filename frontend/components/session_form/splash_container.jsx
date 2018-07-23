1a2d300c7c5ff4438d461382c8b12a14e094931f
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-23 10:28:35 -0700

    reduce # of demo users to make it easier to seed data

--- frontend/components/session_form/splash_container.jsx
+++ frontend/components/session_form/splash_container.jsx
@@ -6,22 +6,22 @@ import Splash from './splash';
 const DEMO = {
   0: 'Jamie Lannister',
   1: 'Tyrion Lannister',
-  2: 'Jon Snow',
-  3: 'Arya Stark',
-  4: 'Oberyn Martell',
-  5: 'Khal Drogo',
-  6: 'Sansa Stark',
-  7: 'Margaery Tyrell',
-  8: 'Bran Stark',
-  9: 'Ramsay Bolton',
-  10: 'Jorah Mormont',
-  11: 'Samwell Tarly',
-  12: 'Tommen Baratheon',
-  13: 'Davos Seaworth'
+  2: 'Jon Snow'
+  // 3: 'Arya Stark',
+  // 4: 'Oberyn Martell',
+  // 5: 'Khal Drogo',
+  // 6: 'Sansa Stark',
+  // 7: 'Margaery Tyrell',
+  // 8: 'Bran Stark',
+  // 9: 'Ramsay Bolton',
+  // 10: 'Jorah Mormont',
+  // 11: 'Samwell Tarly',
+  // 12: 'Tommen Baratheon',
+  // 13: 'Davos Seaworth'
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  user: {user_name: DEMO[Math.floor(Math.random()*14)], password: 'demoUser'},
+  user: {user_name: DEMO[Math.floor(Math.random()*3)], password: 'demoUser'},
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
